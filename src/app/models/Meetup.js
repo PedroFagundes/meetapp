da8bd8fd0c498384bd3b27bf7a279773534d89da
Pedro Fagundes <pedrofagundesb@gmail.com>
2019-09-01 10:39:56 -0300

    Creates Meetup migration and model files

---
+++ src/app/models/Meetup.js
@@ -0,0 +1,24 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Meetup extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        title: Sequelize.STRING,
+        description: Sequelize.TEXT,
+        location: Sequelize.STRING,
+        date: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id' });
+    this.belongsTo(models.File, { foreignKey: 'file_id' });
+  }
+}
+
+export default Meetup;
